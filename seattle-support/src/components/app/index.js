6d8fb490024c52e06fab1affc11c313ea3e8862e
ginsueddy <ginsu.eddy@gmail.com>
2021-12-03 16:40:31 -0800

    got canvas to render

--- seattle-support/src/components/app/index.js
+++ seattle-support/src/components/app/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import localforage from 'localforage';
 import Home from '../screens/Home';
+import { createStore } from 'polotno/model/store';
 import Pamphlets from '../screens/Pamphlets';
+
+const store = createStore({ key: '1dhazL_sUsyRbsXfvCai' });
+window.store = store;
+
+localforage.getItem('polotno-state', function (err, json) {
+  if (json) {
+    store.loadJSON(json);
+  }
+  if (!store.pages.length) {
+    store.addPage();
+  }
+});
+
+store.on('change', () => {
+  try {
+    const json = store.toJSON();
+    localforage.setItem('polotno-state', json);
+  } catch (e) {}
+});
 
 const App = () => {
     return (
         <Router>
             <Routes>
                 <Route exact path='/' element={<Home/>} />
-                <Route path='/pamphlets' element={<Pamphlets />} />
+                <Route path='/pamphlets' element={<Pamphlets store={store} />} />
             </Routes>
         </Router>
     );
--- seattle-support/src/components/screens/Pamphlets/index.js
+++ seattle-support/src/components/screens/Pamphlets/index.js
@@ -1,37 +1,12 @@
 import React from 'react';
-// import localforage from 'localforage';
-import { createStore } from 'polotno/model/store';
-
 import { PolotnoContainer, SidePanelWrap, WorkspaceWrap } from 'polotno';
 import { Toolbar } from 'polotno/toolbar/toolbar';
 import { ZoomButtons } from 'polotno/toolbar/zoom-buttons';
 import { SidePanel } from 'polotno/side-panel';
 import { Workspace } from 'polotno/canvas/workspace';
-
 import { loadFile } from './file';
 import Topbar from './topbar';
 import withLayout from "../../app/navigation/withLayout";
-
-// import './index.css';
-
-const store = createStore({ key: '1dhazL_sUsyRbsXfvCai' });
-window.store = store;
-
-// localforage.getItem('polotno-state', function (err, json) {
-//   if (json) {
-//     store.loadJSON(json);
-//   }
-//   if (!store.pages.length) {
-//     store.addPage();
-//   }
-// });
-
-// store.on('change', () => {
-//   try {
-//     const json = store.toJSON();
-//     localforage.setItem('polotno-state', json);
-//   } catch (e) {}
-// });
 
 const useHeight = () => {
     const [height, setHeight] = React.useState(window.innerHeight);
@@ -69,11 +44,11 @@ const useHeight = () => {
         <Topbar store={store} />
         <div style={{ height: 'calc(100% - 50px)' }}>
           <PolotnoContainer className="polotno-app-container">
-            {/* <SidePanelWrap>
+            <SidePanelWrap>
               <SidePanel store={store} />
-            </SidePanelWrap> */}
+            </SidePanelWrap>
             <WorkspaceWrap>
-              {/* <Toolbar store={store} /> */}
+              <Toolbar store={store} />
               <Workspace store={store} />
               <ZoomButtons store={store} />
             </WorkspaceWrap>
@@ -84,69 +59,3 @@ const useHeight = () => {
   };
 
 export default withLayout(Pamphlets);
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App store={store} />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-
-
-
-
-
-
-
-
-
-// import React from "react";
-// import withLayout from "../../app/navigation/withLayout";
-// import { createDemoApp } from 'polotno/polotno-app';
-
-// import { PolotnoContainer, SidePanelWrap, WorkspaceWrap } from 'polotno';
-
-// import { Workspace } from 'polotno/canvas/workspace';
-// import { SidePanel } from 'polotno/side-panel';
-// import { Toolbar } from 'polotno/toolbar/toolbar';
-// import { ZoomButtons } from 'polotno/toolbar/zoom-buttons';
-
-// // import all default sections
-// // we will not use all of them in the demo
-// // just to show what we have
-
-// import { DEFAULT_SECTIONS } from 'polotno/side-panel';
-
-// const sections = [...DEFAULT_SECTIONS];
-
-// const Pamphlets = () => {
-//     return (
-//         <PolotnoContainer className="polotno-app-container">
-//         <SidePanelWrap style={{ height: '90vh', width: '35vh' }}>
-//           <SidePanel store={store} sections={sections} />
-//         </SidePanelWrap>
-//         <WorkspaceWrap style={{ height: '90vh', width: '90vh' }}>
-//           <Toolbar store={store} downloadButtonEnabled />
-//           <Workspace store={store} />
-//           <ZoomButtons store={store} />
-//         </WorkspaceWrap>
-//       </PolotnoContainer>
-
-//     );
-// };
-
-// const { store } = createDemoApp({
-//     container: document.getElementById('root'),
-//     key: '1dhazL_sUsyRbsXfvCai',
-//     showCredit: true,
-//   });
-
-
-// store.addPage();
-// store.activePage.addElement({
-//   type: 'text',
-//   text: 'hello',
-// });
-
-// export default withLayout(Pamphlets);
