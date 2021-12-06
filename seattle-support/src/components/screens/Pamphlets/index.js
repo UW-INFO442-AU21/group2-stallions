420cc035081873167b5c83b059a90a65d155c633
ColinKwiecinski <colinkwi@uw.edu>
2021-12-05 21:17:13 -0800

    include templates in editor

--- seattle-support/src/components/screens/Pamphlets/index.js
+++ seattle-support/src/components/screens/Pamphlets/index.js
@@ -7,55 +7,63 @@ import { Workspace } from 'polotno/canvas/workspace';
 import { loadFile } from './file';
 import Topbar from './topbar';
 import withLayout from "../../app/navigation/withLayout";
+import { TemplatesSection } from './template';
+import { ElementsSection, TextSection, BackgroundSection, UploadSection, SizeSection} from 'polotno/side-panel';
 
 const useHeight = () => {
     const [height, setHeight] = React.useState(window.innerHeight);
     React.useEffect(() => {
-      window.addEventListener('resize', () => {
-        setHeight(window.innerHeight);
-      });
+        window.addEventListener('resize', () => {
+            setHeight(window.innerHeight);
+        });
     }, []);
     return height;
-  };
+};
 
-  const Pamphlets = ({ store }) => {
+const sections = [TemplatesSection, ElementsSection, TextSection, BackgroundSection, UploadSection, SizeSection]
+
+const Pamphlets = ({ store }) => {
     const handleDrop = (ev) => {
-      // Prevent default behavior (Prevent file from being opened)
-      ev.preventDefault();
+        // Prevent default behavior (Prevent file from being opened)
+        ev.preventDefault();
 
-      // Use DataTransfer interface to access the file(s)
-      for (let i = 0; i < ev.dataTransfer.files.length; i++) {
-        loadFile(ev.dataTransfer.files[i], store);
-      }
+        // Use DataTransfer interface to access the file(s)
+        for (let i = 0; i < ev.dataTransfer.files.length; i++) {
+            loadFile(ev.dataTransfer.files[i], store);
+        }
     };
-
+    // loadTemplate(store);
     const height = useHeight();
 
     return (
-      <div
-        style={{
-          width: '100vw',
-          height: height + 'px',
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-        onDrop={handleDrop}
-      >
-        <Topbar store={store} />
-        <div style={{ height: 'calc(100% - 50px)' }}>
-          <PolotnoContainer className="polotno-app-container">
-            <SidePanelWrap>
-              <SidePanel store={store} />
-            </SidePanelWrap>
-            <WorkspaceWrap>
-              <Toolbar store={store} />
-              <Workspace store={store} />
-              <ZoomButtons store={store} />
-            </WorkspaceWrap>
-          </PolotnoContainer>
+        <div
+            style={{
+                width: '90vw',
+                height: height + 'px',
+                display: 'flex',
+                flexDirection: 'column',
+            }}
+            onDrop={handleDrop}>
+
+            <h1> Pamphlet Generator</h1>
+            <p>Use this pamhplet generator to create custom informational flyers that can be distributed to people in need who may
+                lack access to technology </p>
+
+            <Topbar store={store} />
+            <div style={{ height: 'calc(90% - 50px)' }}>
+                <PolotnoContainer className="polotno-app-container">
+                    <SidePanelWrap>
+                        <SidePanel store={store} sections={sections} defaultSection="custom-templates" />
+                    </SidePanelWrap>
+                    <WorkspaceWrap>
+                        <Toolbar store={store} />
+                        <Workspace store={store} />
+                        <ZoomButtons store={store} />
+                    </WorkspaceWrap>
+                </PolotnoContainer>
+            </div>
         </div>
-      </div>
     );
-  };
+};
 
 export default withLayout(Pamphlets);
